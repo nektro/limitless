1935af0f0ac595bd0ea0fd25018161f829d19910
Meghan Denny <hello@nektro.net>
2021-09-27 23:26:15 -0700

    add util script that regenerates all os base images

---
+++ generate/_all.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Meta script for building all architectures of an OS
+
+os="$1"
+
+case "$os" in
+"debian")
+    ./generate/debian.sh amd64
+    ./generate/debian.sh i386
+    # ./generate/debian.sh arm64
+    # ./generate/debian.sh armel
+    # ./generate/debian.sh armhf
+    # ./generate/debian.sh mips
+    # ./generate/debian.sh mips64el
+    # ./generate/debian.sh mipsel
+    # ./generate/debian.sh ppc64el
+    # ./generate/debian.sh s390x
+    ;;
+*)
+    echo "Invalid OS chosen."
+    ;;
+esac
