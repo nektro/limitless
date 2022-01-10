77e0e267117a3ca3cf1b5c24df00c584136f50b6
Meghan Denny <hello@nektro.net>
2022-01-10 01:25:12 -0800

    generate/all- use tools/os-list-arches.zig

--- generate/_all.sh
+++ generate/_all.sh
@@ -6,71 +6,8 @@ set -x
 # Meta script for building all architectures of an OS
 
 os="$1"
-archs=""
 
-case "$os" in
-"debian")
-    archs=(
-        'amd64'
-        'i386'
-        # 'arm64'
-        # 'armel'
-        # 'mips64el'
-        # 'ppc64el'
-        # 's390x'
-    )
-    ;;
-"alpine")
-    archs=(
-        'x86_64'
-        # 'x86'
-        # 'aarch64'
-        # 'armv7'
-        # 'ppc64le'
-        # 's390x'
-    )
-    ;;
-"freebsd")
-    archs=(
-        'amd64'
-        # 'arm64-aarch64'
-        # 'i386'
-        # 'powerpc-powerpc64le'
-        # 'riscv-riscv64'
-    )
-    ;;
-"netbsd")
-    archs=(
-        'amd64'
-        # 'evbarm-aarch64'
-        # 'i386'
-        # 'evbarm-earm'
-        # 'evbmips-mips64el'
-        # 'sparc64'
-    )
-    ;;
-"openbsd")
-    archs=(
-        'amd64'
-        # 'arm64'
-        # 'i386'
-        # 'armv7'
-        # 'riscv64'
-        # 'sparc64'
-    )
-    ;;
-"dragonflybsd")
-    archs=(
-        "x86_64"
-    )
-    ;;
-*)
-    echo "Invalid OS chosen."
-    exit
-    ;;
-esac
-
-for arc in ${archs[@]}
+for arc in $(zig run tools/os-list-arches.zig -- "$os")
 do
     ./generate/$os.sh $arc
 done
